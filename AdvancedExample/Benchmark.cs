3375755b9dbb5896361ae5bedb3de14ab197c79a
Константин Дьяченко <konard@me.com>
2020-01-24 19:16:12 +0300

    Added benchmark for 2 tags.

--- AdvancedExample/Benchmark.cs
+++ AdvancedExample/Benchmark.cs
@@ -6,23 +6,36 @@ namespace AdvancedExample
     public class AdvancedExampleBenchmark
     {
         private readonly ObjectsAndTagsStorage _storage;
-        private readonly uint[] _tags;
+        private readonly uint[] _tags2;
+        private readonly uint[] _tags5;
 
         public AdvancedExampleBenchmark()
         {
             _storage = new ObjectsAndTagsStorage(@"F:\DataArchive\db.links");
             _storage.InitMarkers();
             _storage.GenerateData(100000000, 10000, 10);
-            _tags = new uint[] { _storage.GetTag(), _storage.GetTag(), _storage.GetTag(), _storage.GetTag(), _storage.GetTag() };
+            _tags2 = new uint[] { _storage.GetTag(), _storage.GetTag() };
+            _tags5 = new uint[] { _storage.GetTag(), _storage.GetTag(), _storage.GetTag(), _storage.GetTag(), _storage.GetTag() };
         }
 
         [Benchmark]
-        public List<uint> GetObjectsByTags()
+        public List<uint> GetObjectsBy2Tags()
         {
             List<uint> result = null;
             for (int i = 0; i < 10; i++)
             {
-                result = _storage.GetObjectsByAnyTags(_tags);
+                result = _storage.GetObjectsByAnyTags(_tags2);
+            }
+            return result;
+        }
+
+        [Benchmark]
+        public List<uint> GetObjectsBy5Tags()
+        {
+            List<uint> result = null;
+            for (int i = 0; i < 10; i++)
+            {
+                result = _storage.GetObjectsByAnyTags(_tags5);
             }
             return result;
         }
